58e296b1c88f372a7ea99658b4a51e49a9d162c2
myd <1353795848@qq.com>
2017-04-11 21:15:53 +0800

    修改client

--- QuoteOfTheMoment/src/main/java/qotm/QuoteOfTheMomentClientHandler.java
+++ QuoteOfTheMoment/src/main/java/qotm/QuoteOfTheMomentClientHandler.java
@@ -1,7 +1,26 @@
 package qotm;
 
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+
 /**
  * Created by 301 on 2017/4/11.
  */
-public class QuoteOfTheMomentClientHandler {
+public class QuoteOfTheMomentClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+        String response = msg.content().toString(CharsetUtil.UTF_8);
+        if(response.startsWith("QOTM: ")){
+            System.out.println("Quote of the moment: "+ response.substring(6));
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }

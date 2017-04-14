ada41c91d29aae16068289944960f61dd72bb736
myd <1353795848@qq.com>
2017-04-14 09:19:59 +0800

    完成SecureChat

--- SecureChat/src/main/java/securechat/SecureChatClient.java
+++ SecureChat/src/main/java/securechat/SecureChatClient.java
@@ -33,8 +33,31 @@ public class SecureChatClient {
             //Start the connection attempt.
             Channel ch = b.connect(HOST, PORT).sync().channel();
             //Read commands from the stdin.
-            ChannelFuture lastWriteFUture = null;
+            ChannelFuture lastWriteFuture = null;
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            for(;;) {
+                String line = in.readLine();
+                if(line == null) {
+                    break;
+                }
+
+                //Sends the received line to the server.
+                lastWriteFuture = ch.writeAndFlush(line + "\r\n");
+
+                //If user typed the 'bye' command, wait until the server closes
+                //the connection
+                if("bye".equals(line.equals(line.toLowerCase()))) {
+                    ch.closeFuture().sync();
+                    break;
+                }
+            }
+
+            //Wait until all messages are flushed before closing the channel.
+            if(lastWriteFuture != null) {
+                lastWriteFuture.sync();
+            }
+        }finally {
+            group.shutdownGracefully();
         }
     }
 }

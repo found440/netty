88437e208aac68e01f895697656c02914abd1590
myd <1353795848@qq.com>
2017-04-14 10:39:28 +0800

    ObjectEchoClient

---
+++ ObjectEcho/src/main/java/objectecho/ObjectEchoServerHandler.java
@@ -0,0 +1,27 @@
+package objectecho;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created by 301 on 2017/4/14.
+ */
+public class ObjectEchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}

98c77cfcab5e56b9b47eb75ac75a494713c65553
myd <1353795848@qq.com>
2017-04-14 16:15:25 +0800

    factorial

---
+++ factorial/src/main/java/FactorialClient.java
@@ -0,0 +1,48 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+/**
+ * Created by 301 on 2017/4/14.
+ */
+public final class FactorialClient {
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8332"));
+    static final int COUNT = Integer.parseInt(System.getProperty("count", "100"));
+
+    public static void main(String []args) throws Exception {
+        //Configure SSL;
+        final SslContext sslCtx;
+        if(SSL) {
+            sslCtx = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new FactorialClientInitializer(sslCtx));
+            //Make a new connection.
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+
+            //Get the handler instance to retrieve the answer.
+            FactorialClientHandler handler =
+                    (FactorialClientHandler) f.channel().pipeline().last();
+            //Print out the answer.
+            System.err.format("Factorial of %d is : %d\n", COUNT, handler.getFactorial());
+
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+}
